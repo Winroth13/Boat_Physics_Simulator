9358d3ebf78a3f1e0e921bfbc20c0905ef06d16e
Winroth13 <emil.winroth@gmail.com>
2026-05-14 10:41:05 +0200

    Remvoed Redundant Shadow Shaders

--- Boat_Physics_Simulator/include/core/renderer/renderer.h
+++ Boat_Physics_Simulator/include/core/renderer/renderer.h
@@ -318,17 +318,12 @@ private:
 	std::vector<SpotLightData> mSpotLightsData;
 
 	/* Shadow Mapping */
-	std::unique_ptr<VertexShader> mShadowMapVertexShader;
 	std::unique_ptr<PixelShader> mShadowMapPixelShader;
 	std::unique_ptr<PixelShader> mShadowMapLinearPixelShader;
-
-	std::unique_ptr<HullShader> mShadowTessellationHullShader;
-	std::unique_ptr<DomainShader> mShadowDisplacementDomainShader;
 
 	ShadowMap mDirectionalLightsShadowMap;
 	OmnidirectionalShadowMap mPointLightsShadowMap;
 	ShadowMap mSpotLightsShadowMap;
-	ID3D11InputLayout* mShadowInputLayout = nullptr;
 	ID3D11SamplerState* mShadowMapSampler = nullptr;
 
 	/* Particle System */
